cab1d88d3acfbe9e08cae2c4a82fbb8d93d2add1
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-08-26 10:44:50 +0200

    example/get-to-post0,wiki0: minimalistic alternatives

---
+++ example/get-to-post0.php
@@ -0,0 +1,1 @@
+<html><body><form action=. method=post><input type=text id=p name=p><input type=submit value=post></form><script>document.getElementById("p").value=window.location.hash.substring(1);document.forms[0].submit();</script></body></html>
---
+++ example/wiki0.php
@@ -0,0 +1,1 @@
+<html><body><form method=post><textarea name=o><?php if(isset($_POST['w']))fwrite(fopen('w','w'),htmlspecialchars($_POST['o']),4096);readfile('w')?></textarea><input type=submit name=w value=submit><input type=submit value=get><input type=text name=p value=<?php echo $p?>></form></body></html>
