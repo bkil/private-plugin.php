4e912962521c241d954e1078206c8f92eea62938
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-09-21 13:49:09 +0200

    publish-pic-be: typo in if condition

--- example/publish-pic-be.php
+++ example/publish-pic-be.php
@@ -13,7 +13,7 @@ foreach (glob('*') as $n)
   $b += lstat($n)[12];
 
 // Simultaneously check 64-100kB upload and <32-50MB quota
-if ($f['size'] | $b < 1e5)
+if (($f['size'] | $b) < 1e5)
   if (mime_content_type($t) == 'image/jpeg')
     // We split the function name with `eval` as a workaround so that their regexp won't find it
     // "COMODO WAF: PHP Injection Attack - Matched Data: move_uploaded_file found within ARGS"
