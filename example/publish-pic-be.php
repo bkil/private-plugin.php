d43fefb91d7095a1e416b04c5669d9323dac6d11
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-09-21 19:35:07 +0200

    publish-pic-be: golf away quotes around strings, getimagesize()

--- example/publish-pic-be.php
+++ example/publish-pic-be.php
@@ -1,20 +1,21 @@
 <?php
 
+// 1e5 seconds is ~1.16 days
 $m = time() / 1e5 % 3;
 foreach(glob((($m + 1) % 3) . '*') as $n)
   unlink($n);
 
-$f = $_FILES['f'];
-$t = $f['tmp_name'];
+$f = $_FILES[f]; // ['f']
+$t = $f[tmp_name]; // ['tmp_name']
 $m .= sha1_file($t) . '.jpg';
 
 $b = 0;
 foreach (glob('*') as $n)
   $b += lstat($n)[12];
 
-// Simultaneously check 64-100kB upload and <32-50MB quota
-if (($f['size'] | $b) < 1e5)
-  if (mime_content_type($t) == 'image/jpeg')
+// Simultaneously check <64-100kB uploaded file size and <32-50MB disk quota
+if (($f[size] | $b) < 1e5) // ['size']
+  if (getimagesize($t))
     // We split the function name with `eval` as a workaround so that their regexp won't find it
     // "COMODO WAF: PHP Injection Attack - Matched Data: move_uploaded_file found within ARGS"
     if (eval('return(move_uploaded'."_file('$t','$m'));"))
