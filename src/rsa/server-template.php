6238a357502a4e83200098c9f906480bedb3550b
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-08-23 23:03:46 +0200

    rsa/server-template.php: eliminate $k variable

--- src/rsa/server-template.php
+++ src/rsa/server-template.php
@@ -1,10 +1,6 @@
 <?php
 
-$k = <<<K
-((author_pub))
-K;
-
-if (!isset($_REQUEST['p']) || (!$p = $_REQUEST['p']) || !isset($_REQUEST['s']) || (!$s = $_REQUEST['s']) || (1 !== openssl_verify($p, base64_decode($s), $k, ((signature_digest))))) {
+if (!isset($_REQUEST['p']) || (!$p = $_REQUEST['p']) || !isset($_REQUEST['s']) || (!$s = $_REQUEST['s']) || (1 !== openssl_verify($p, base64_decode($s), '((author_pub))', ((signature_digest))))) {
   header('HTTP/1.0 400 Bad Request');
   header('Content-Type: text/plain');
   readfile(__FILE__);
