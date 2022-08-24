a8e9b15ce17f18e27e708c2c10668c7885bbd926
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-08-24 02:33:06 +0200

    example/chat-fs.php: support name field, golf out $R['c'], {}

--- example/chat-fs.php
+++ example/chat-fs.php
@@ -1,22 +1,23 @@
 <?php
 $R = $_REQUEST;
+$n = isset($R['n']) ? preg_replace('/\W/', 0, $R['n']) : 'name';
 
 flock($f = fopen(9, 'c+'), LOCK_EX);
 
 $d = file_get_contents(9);
-if (isset($R['c']))
-  if ($R['c']) {
-    if (file_put_contents(
-      8,
-      $d = substr(
-        $d .
-        date("\nD H:i ") .
-        htmlspecialchars(
-          preg_replace('/[[:cntrl:]]/', 0, $R['c'])),
-        -1e4))
-      )
-      rename(8, 9);
-  }
+if ($c = isset($R['c']) ? $R['c'] : 0)
+  if (file_put_contents(
+    8,
+    $d = substr(
+      $d .
+      date("\nD H:i ") .
+      $n .
+      '|' .
+      htmlspecialchars(
+        preg_replace('/[[:cntrl:]]/', 0, $c)),
+      -1e4))
+    )
+    rename(8, 9);
 
 flock($f, LOCK_UN);
 fclose($f);
@@ -24,6 +25,6 @@ fclose($f);
 echo '<!DOC' .
   'TYPE html><html><head><title>Chat</title><li' .
   'nk rel="shortcut icon" type=image/x-icon href=data:image/x-icon;,><me' .
-  "ta charset=utf-8><body><pre>$d</pre><form action=? method=post><input name=c autofocus maxlength=99><input type=submit value=Comment/Refresh><input type=hidden value=$s name=s><input type=hidden name=p value=\"" .
+  "ta charset=utf-8><body><pre>$d</pre><form action=? method=post><input value=$n name=n><input name=c autofocus maxlength=99><input type=submit value=Comment/Refresh><input type=hidden value=$s name=s><input type=hidden name=p value=\"" .
   htmlspecialchars($p) .
   '">';
