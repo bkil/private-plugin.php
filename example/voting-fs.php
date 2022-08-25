fbed70ebec6bcb9bd5a4efc48302229358b8e839
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-08-25 23:22:31 +0200

    example/voting-fs.php: display days remaining until poll deletion

--- example/voting-fs.php
+++ example/voting-fs.php
@@ -1,9 +1,9 @@
 <?php
-$R=$_REQUEST;
+$R = $_REQUEST;
 // 1e5 seconds is ~1.16 days
-$e = time() / 1e5 % 9;
-// cleanup old polls after ~10 days
-foreach(glob((($e + 1) % 9) . '*') as $g)
+$m = time() / 1e5 % 9;
+// cleanup old polls after ~9 days
+foreach(glob((($m + 1) % 9) . '*') as $g)
   unlink($g);
 
 // break some HTML tags to avoid Comodo WAF firewall warnings
@@ -12,19 +12,16 @@ echo '<!DOC' . 'TYPE html><html><head><me' .
   'nk rel="shortcut icon" type=image/x-icon href=data:image/x-icon;,><s' .
   'tyle>input,label{display:block}</style><body><form action=? method=post>';
 
-if (isset($R['o']) && isset($R['e']) && preg_match('/^[0-8]$/', $R['e'])) {
-  // new poll: o
-  $o = $R['o'];
-
+if (isset($R['o']) && isset($R['e']) && preg_match('/^[0-8]$/', $e = $R['e'])) {
   // get votes on poll: o e
-  $e =  $R['e'];
+  $o = $R['o'];
 
   $h = '<a href="?p=' . urlencode($p) . '&s=' . urlencode($s);
   echo $h . '">New poll</a><table><tr>';
   $O = '';
   $C = [];
   foreach ($o as $i => $q) {
-    // stop when seeing first empty choice
+    // stop after seeing first empty choice
     if (!$q) {
       $o = array_slice($o, 0, $i);
       break;
@@ -77,13 +74,14 @@ if (isset($R['o']) && isset($R['e']) && preg_match('/^[0-8]$/', $R['e'])) {
     echo '><td><input name=v[] ' .
     ($i ? 'type=checkbox value=' . $i : 'required');
 
-  echo '></table><input type=submit value=Vote>';
+  echo '></table>' . -($m - $e - 8) % 9 . ' days left<input type=submit value=Vote>';
   echo "$h$O&e=$e\">Share link</a>";
 } else {
   // HTML index to create new poll
   echo '<input type=submit value=CreatePoll><label>Description<input required name=o[]></label><label>Choices</label><input required name=o[]>';
   for ($i=7; $i--;)
     echo '<input name=o[]>';
+  $e = $m;
 }
 
 echo '<input type=hidden name=p value="' . htmlspecialchars($p) .
