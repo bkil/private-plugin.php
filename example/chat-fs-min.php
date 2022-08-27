75cf1ad8cc4dab5d4769b4c60b189c727dfbc99c
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-08-27 12:17:54 +0200

    example/chat-fs-min: fewer notices by using numeric filename

--- example/chat-fs-min.php
+++ example/chat-fs-min.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents(w, $d = substr(file_get_contents(w) . date("\nD H:i ") . htmlspecialchars($_REQUEST[c]), -1e4));
+file_put_contents(9, $d = substr(file_get_contents(9) . date("\nD H:i ") . htmlspecialchars($_REQUEST[c]), -1e4));
 
 echo '<!DOC' .
   "TYPE html><html><body><pre>$d</pre><form><input name=c><input type=submit><input name=s value=$s><input name=p value=\"" .
