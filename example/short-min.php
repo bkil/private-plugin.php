1ed4f159b0be1ab001f6133ddb5b9dea8f8e4441
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-09-12 04:32:10 +0200

    example/short,short-min: add

---
+++ example/short-min.php
@@ -0,0 +1,15 @@
+<?php
+// 1e5 seconds is ~1.16 days
+// clean up old data after ~4 days
+foreach (glob((($m = time() / 1e5 & 7) ^ 4) . '*') as $g)
+  unlink($g);
+
+if (preg_match('/^\w+$/', $u = $_REQUEST[u]))
+  header('Location: ' . file_get_contents($u));
+
+foreach (glob('*') as $g)
+  $b += lstat($g)[12];
+
+if ($b < 1e4)
+  if (preg_match('/^https?:[ -~]+$/', $u))
+    file_put_contents($m . sha1($u), $u);
---
+++ example/short.php
@@ -0,0 +1,21 @@
+<?php
+// 1e5 seconds is ~1.16 days
+// clean up old data after ~4 days
+foreach (glob((($m = time() / 1e5 & 7) ^ 4) . '*') as $g)
+  unlink($g);
+
+$R = $_REQUEST;
+if (isset($R['u'])) {
+  if (preg_match('/^\w+$/', $u = $R['u']))
+    header('Location: ' . @file_get_contents($u));
+  else if (preg_match('/^https?:[ -~]{1,9000}$/', $u)) {
+    $b = 0;
+    foreach (glob('*') as $g)
+      $b += lstat($g)[12];
+
+    if ($b < 1e4)
+      file_put_contents($u = $m . sha1($u), $u);
+
+    echo $u;
+  }
+}
