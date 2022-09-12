0b5fd14873183621ba06aaabc92808a15df0bec4
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-09-12 04:17:22 +0200

    dsa/server-template: mkdir/chdir $s

--- src/dsa/server-template.php
+++ src/dsa/server-template.php
@@ -1,6 +1,15 @@
 <?php
 
-if (!isset($_REQUEST['p']) || (!$p = $_REQUEST['p']) || !isset($_REQUEST['s']) || (!$s = $_REQUEST['s']) || (1 !== openssl_verify($p, base64_decode($s), '((author_pub))', ((signature_digest))))) {
+if (!isset($_REQUEST['p']) || (!$p = $_REQUEST['p']) ||
+    !isset($_REQUEST['s']) || (!$s = $_REQUEST['s']) ||
+    (1 !== openssl_verify($p, base64_decode($s), '((author_pub))', ((signature_digest)))) ||
+    (
+      ($s = str_replace('+', '-', $s)) &
+      ($s = str_replace('/', '_', $s)) &
+      ($s = preg_replace('/[^0-9A-Za-z_-]/', '', $s)) &
+      (@mkdir($s) | 1) &
+      !chdir($s)
+    )) {
   header('HTTP/1.0 400 Bad Request');
   header('Content-Type: text/plain');
   readfile(__FILE__);
