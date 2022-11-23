bf9bd2c9b18ea860274f962b097d3363cb8e4717
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-11-23 23:34:06 +0100

    example/voting-fs-min.php: fix disk usage in empty directory

--- example/voting-fs-min.php
+++ example/voting-fs-min.php
@@ -25,7 +25,7 @@ if (preg_match('/^[0-8]$/', $R[e])) {
     $b -= lstat($g)[12];
 
   $V = json_decode(file_get_contents($f = $e . sha1($O)));
-  if ($b > -1e3)
+  if (1e3 > -$b)
     if ($R[v][0]) {
       // place vote on a poll: o e v
       $V[] = $R[v];
