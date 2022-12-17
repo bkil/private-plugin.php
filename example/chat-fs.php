697f4c6f4d0f4c8f14159c154f16958001845dea
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-12-17 17:18:29 +0100

    example/chat-fs.php: minify via coalescing operator

--- example/chat-fs.php
+++ example/chat-fs.php
@@ -1,16 +1,16 @@
 <?php
 $R = $_REQUEST;
-$n = isset($R['n']) ? preg_replace('/\W/', 0, $R['n']) : 'name';
+$n = preg_replace('/\W/', 0, $R['n'] ?? 'name');
 
 // split to avoid COMODO WAF warning
 flock($f = eval('return fop' . 'en(9,"c+");'), LOCK_EX);
 
 $d = file_get_contents(9);
-if ($c = isset($R['c']) ? $R['c'] : 0)
+if ($c = $R['c'] ?? 0)
   if (file_put_contents(
     8,
 // split to avoid COMODO WAF warning
-    $d = eval('return sub' . 'str($d.date("\nD H:i ").$n."|".htmlspecialchars(preg_replace("/[[:cntrl:]]/",0,$c)),-1e4);')
+    $d = eval('return sub' . 'str($d.date("\nD H:i ").$n.":".htmlspecialchars(preg_replace("/[[:cntrl:]]/",0,$c)),-1e4);')
     ))
     rename(8, 9);
 
